d967a88dde079e195cd809761997da2b78dff4e8
sagarrakshe <sagarrakshe2@gmail.com>
2013-10-29 10:13:45 +0530

    created install.sh file

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+# install.sh
+# Copyright (C) 2013 sagar <sagar@sagar-liquid>
+#
+
+
+echo "Installing freeglut..."
+sudo apt-get install freeglut3-dev
+
+echo "Installing SDL..."
+sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev
